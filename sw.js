9a566b15e318a1442f716b7cf6003abdbd7a9e1d
Galikin <galius2002@gmail.com>
2026-06-29 18:04:49 +1000

    First upload

---
+++ sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'flying-tickets-v1';
+const urlsToCache = [
+  './',
+  './flying_tickets_tracker.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache).catch(() => {
+        // Ignore cache errors for offline-first
+        return Promise.resolve();
+      });
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).catch(() => {
+        // Return cached version or offline page
+        return caches.match('./flying_tickets_tracker.html');
+      });
+    })
+  );
+});
